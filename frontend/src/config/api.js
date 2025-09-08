9ab69c565fc6759ed85575f899f97298a0f9d07d
HarrisAmin <harrisaminjutt@gmail.com>
2025-09-08 14:07:56 +0500

    final fix

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: `${import.meta.env.API_VITE_URL}/api/v1`,
   timeout: 120000,
   headers: {
     "Content-Type": "application/json",
